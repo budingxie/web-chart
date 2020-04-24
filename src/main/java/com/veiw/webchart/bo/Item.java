afc795d05a95c88492785d06041db42712a1d058
pengyou <pengyou@xiaomi.com>
2020-04-24 11:30:54 +0800

    调整

---
+++ src/main/java/com/veiw/webchart/bo/Item.java
@@ -0,0 +1,23 @@
+package com.veiw.webchart.bo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author pengyou@xiaomi.com
+ * @date 2020/4/24
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    private String time;
+
+    private String hexPacket;
+
+    private String length;
+
+    //....
+}
